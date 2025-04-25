14ac2d895bc94f4b29df0214cd782f3f4f241fe6
BERA UYGUR <230541107@firat.edu.tr>
2025-04-25 20:27:07 +0300

    Port numarası sabit 3000 olarak ayarlandı

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor`);
 }); 
